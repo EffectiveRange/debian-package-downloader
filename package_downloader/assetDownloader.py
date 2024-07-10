f5fef35153853879035a4c048aa5c37a74015213
Attila Gombos <attila.gombos@effective-range.com>
2024-07-10 12:01:53 +0200

    Add support downloading multiple matching assets from one release

--- package_downloader/assetDownloader.py
+++ package_downloader/assetDownloader.py
@@ -16,7 +16,8 @@ log = get_logger('AssetDownloader')
 
 class IAssetDownloader(object):
 
-    def download(self, config: ReleaseConfig, git_release: GitRelease, skip_if_exists: bool = True) -> str:
+    def download(self, config: ReleaseConfig, release: GitRelease,
+                 first_match_only: bool = False, skip_if_exists: bool = True) -> list[str]:
         raise NotImplementedError()
 
 
@@ -25,19 +26,28 @@ class AssetDownloader(IAssetDownloader):
     def __init__(self, file_downloader: IFileDownloader) -> None:
         self._file_downloader = file_downloader
 
-    def download(self, config: ReleaseConfig, release: GitRelease, skip_if_exists: bool = True) -> str:
+    def download(self, config: ReleaseConfig, release: GitRelease,
+                 first_match_only: bool = False, skip_if_exists: bool = True) -> list[str]:
         assets = release.get_assets()
 
         log.debug('Retrieved asset list', release=config, assets=[asset.name for asset in assets])
+
+        downloaded_files = []
 
         for asset in assets:
             if fnmatch.fnmatch(asset.name, config.matcher):
                 log.info('Found matching asset', release=config, asset=asset.name)
 
-                return self._download_asset(asset, config.get_token())
+                downloaded_files.append(self._download_asset(asset, config.get_token()))
 
-        log.error('No matching asset found', release=config, assets=[asset.name for asset in assets])
-        raise ValueError('No matching asset found')
+                if first_match_only:
+                    break
+
+        if not downloaded_files:
+            log.error('No matching asset found', release=config, assets=[asset.name for asset in assets])
+            raise ValueError('No matching asset found')
+
+        return downloaded_files
 
     def _download_asset(self, asset: GitReleaseAsset, token: Optional[str] = None) -> str:
         log.debug('Downloading asset', asset=asset.name)
