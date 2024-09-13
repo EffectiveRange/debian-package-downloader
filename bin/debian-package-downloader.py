026e6ecaa24575321bf33875590771825767d219
Attila Gombos <attila.gombos@effective-range.com>
2024-09-13 11:04:16 +0200

    Update JsonLoader import

--- bin/debian-package-downloader.py
+++ bin/debian-package-downloader.py
@@ -7,7 +7,8 @@
 import os
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, Namespace
 
-from common_utility import JsonLoader, SessionProvider, FileDownloader
+from common_utility import SessionProvider, FileDownloader
+from common_utility.jsonLoader import JsonLoader
 from context_logger import get_logger, setup_logging
 
 from package_downloader import DebDownloader, RepositoryProvider, AssetDownloader, PackageDownloader
