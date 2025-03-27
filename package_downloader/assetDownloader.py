f6c4d4436a46313320d0d9471dfeb587194de301
AttilaGombosER <attila.gombos@effective-range.com>
2025-03-27 16:23:05 +0100

    Download package and copy for all distros (#13)

--- package_downloader/assetDownloader.py
+++ package_downloader/assetDownloader.py
@@ -18,9 +18,8 @@ log = get_logger('AssetDownloader')
 
 class IAssetDownloader(object):
 
-    def download(
-        self, config: ReleaseConfig, release: GitRelease, first_match_only: bool = False, skip_if_exists: bool = True
-    ) -> list[str]:
+    def download(self, config: ReleaseConfig, release: GitRelease, first_match_only: bool = False,
+                 skip_if_exists: bool = True) -> list[str]:
         raise NotImplementedError()
 
 
@@ -28,11 +27,10 @@ class AssetDownloader(IAssetDownloader):
 
     def __init__(self, file_downloader: IFileDownloader, distro_map: Optional[OrderedDict[str, str]] = None) -> None:
         self._file_downloader = file_downloader
-        self._distro_map = distro_map
+        self._distro_map = distro_map if distro_map else {}
 
-    def download(
-        self, config: ReleaseConfig, release: GitRelease, first_match_only: bool = False, skip_if_exists: bool = True
-    ) -> list[str]:
+    def download(self, config: ReleaseConfig, release: GitRelease, first_match_only: bool = False,
+                 skip_if_exists: bool = True) -> list[str]:
         assets = release.get_assets()
 
         log.debug('Retrieved asset list', release=config, assets=[asset.name for asset in assets])
@@ -43,7 +41,15 @@ class AssetDownloader(IAssetDownloader):
             if fnmatch.fnmatch(asset.name, config.matcher):
                 log.info('Found matching asset', release=config, asset=asset.name)
 
-                downloaded_files.append(self._download_asset(asset, config.raw_token))
+                headers = {'Accept': 'application/octet-stream'}
+
+                if config.raw_token:
+                    headers['Authorization'] = f'token {config.raw_token}'
+
+                if self._distro_map:
+                    downloaded_files.extend(self._download_for_distro(asset, headers))
+                else:
+                    downloaded_files.append(self._file_downloader.download(asset.url, asset.name, None, headers))
 
                 if first_match_only:
                     break
@@ -54,23 +60,9 @@ class AssetDownloader(IAssetDownloader):
 
         return downloaded_files
 
-    def _download_asset(self, asset: GitReleaseAsset, token: Optional[str] = None) -> str:
-        log.debug('Downloading asset', asset=asset.name)
+    def _download_for_distro(self, asset: GitReleaseAsset, headers: dict[str, str]) -> list[str]:
+        for distro_matcher, distro_dir in self._distro_map.items():
+            if distro_matcher in asset.name:
+                return [self._file_downloader.download(asset.url, asset.name, distro_dir, headers)]
 
-        headers = {'Accept': 'application/octet-stream'}
-
-        if token:
-            headers['Authorization'] = f'token {token}'
-
-        sub_dir = None
-
-        if self._distro_map:
-            for distro_matcher, distro_dir in self._distro_map.items():
-                if distro_matcher in asset.name:
-                    sub_dir = distro_dir
-                    break
-            if not sub_dir:
-                sub_dir = list(self._distro_map.values())[0]
-                log.warning('No matching distro found in file name, using default', asset=asset.name, distro=sub_dir)
-
-        return self._file_downloader.download(asset.url, asset.name, sub_dir, headers)
+        return self._file_downloader.download_and_copy(asset.url, list(self._distro_map.values()), asset.name, headers)
